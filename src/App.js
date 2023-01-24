74325d2914ca40f765e573f7cddb70fb49ff7bc1
Arman0701 <gittadevosyan889@gmail.com>
2023-01-24 23:23:17 +0400

    feat(): Project initialization and insertion of basic functionality.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+import convertSeconds from "./helpers/convertSeconds";
+
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [charging, setCharging] = useState(false);
+    const [chargingTime, setChargingTime] = useState(0);
+    const [dischargingTime, setDischargingTime] = useState(0);
+    const [level, setLevel] = useState(0);
+
+    function getBatteryInfo() {
+        navigator.getBattery().then((battery) => {
+            function updateAllBatteryInfo() {
+                updateChargeInfo();
+                updateLevelInfo();
+                updateChargingInfo();
+                updateDischargingInfo();
+            }
+            updateAllBatteryInfo();
+			
+            battery.addEventListener("chargingchange", () => {
+                updateChargeInfo();
+				updateChargingInfo();
+
+            });
+			battery.addEventListener("levelchange", () => {
+				updateLevelInfo();
+			});
+			battery.addEventListener("chargingtimechange", () => {
+				updateChargingInfo();
+			});
+			battery.addEventListener("dischargingtimechange", () => {
+				updateDischargingInfo();
+			});
+            function updateChargeInfo() {
+                setCharging(battery.charging);
+            }
+			
+            function updateLevelInfo() {
+				setLevel(battery.level);
+            }
+			
+            function updateChargingInfo() {
+				setChargingTime(battery.chargingTime);
+            }
+			
+            function updateDischargingInfo() {
+				setDischargingTime(battery.dischargingTime);
+            }
+        });
+    }
+
+    useEffect(() => {
+        getBatteryInfo();
+
+        // eslint-disable-next-line
+    }, []);
+
+    return (
+        <div className="wrapper">
+            <h3>Battery Info Board</h3>
+
+            {!charging && (
+				<p>Discharging after {convertSeconds(dischargingTime)}</p>
+				)}
+            <p>Level: {(level * 100)}%</p>
+			{charging && <>
+				<p>Charging now</p>
+				<p>Charging time: {chargingTime}</p>
+			</>}
+        </div>
+    );
 }
 
 export default App;
