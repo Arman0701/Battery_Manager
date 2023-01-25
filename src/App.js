f00c30db694c4826407ab500363c50358b9902e0
Arman0701 <gittadevosyan889@gmail.com>
2023-01-25 14:54:08 +0400

    feat(): Making UI.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import useBattery from "./hooks/useBattery";
-
-import "./App.scss";
 import convertSeconds from "./helpers/convertSeconds";
 
-function App() {
-	const {
-		charging,
-		chargingTime,
-		dischargingTime,
-		level,
-		
-		setOnChargingChange,
-        setOnDischargingChange,
-        setOnChargingTimeChange,
-        setOnLevelChange,
-	} = useBattery();
+import "./App.scss";
+
+export default function App() {
+    const {
+        charging,
+        chargingTime,
+        dischargingTime,
+        level
+    } = useBattery();
 
     return (
         <div className="wrapper">
-            <h3>Battery Info Board</h3>
-			
-			<p>Charging State: {charging.toString()}</p>
-			<p>Charging Time: {convertSeconds(chargingTime)}</p>
-			<p>Discharging Time: {convertSeconds(dischargingTime)}</p>
-			<p>Level: {level}%</p>
-			
+            <h3>Battery status</h3>
+
+			<div className="battery">
+				<div className={`battery-bg ${charging ? "charging" : ""}`} style={{
+					width: `${level}%`
+				}}></div>
+				<div className="battery-tail"></div>
+				<span>{level}%</span>
+			</div>
+
+			{charging && chargingTime !== Infinity && 
+				<span>
+					Charging time {convertSeconds(chargingTime)}
+				</span>
+			}
+			{charging && dischargingTime !== Infinity && 
+				<span>
+					Time remaining {convertSeconds(dischargingTime)}
+				</span>
+			}
         </div>
     );
 }
-
-export default App;
