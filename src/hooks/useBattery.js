13bd1a43fdf345a4d348ddc1891b30639d81da44
Arman0701 <gittadevosyan889@gmail.com>
2023-01-25 15:59:19 +0400

    fix(): Fixing some styles.

--- src/hooks/useBattery.js
+++ src/hooks/useBattery.js
@@ -1,22 +1,16 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function useBattery() {
     const [charging, setCharging] = useState(false);
     const [chargingTime, setChargingTime] = useState(0);
     const [dischargingTime, setDischargingTime] = useState(0);
     const [level, setLevel] = useState(0);
-    const [onChargingChange, setOnChargingChange] = useState();
-    const [onDischargingChange, setOnDischargingChange] = useState();
-    const [onChargingTimeChange, setOnChargingTimeChange] = useState();
-    const [onLevelChange, setOnLevelChange] = useState();
 
     navigator.getBattery().then((battery) => {
         setCharging(battery.charging);
         setChargingTime(battery.chargingTime);
         setDischargingTime(battery.dischargingTime);
         setLevel(Math.round(battery.level * 100));
-
-		console.log('Log battery ::: ', battery)
 
         battery.addEventListener("chargingchange", () => {
 			setCharging(battery.charging) /// 
@@ -36,11 +30,6 @@ export default function useBattery() {
         charging,
         chargingTime,
         dischargingTime,
-        level,
-
-        setOnChargingChange,
-        setOnDischargingChange,
-        setOnChargingTimeChange,
-        setOnLevelChange,
+        level
     };
 }
